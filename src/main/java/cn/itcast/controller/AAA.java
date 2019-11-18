6a4c9d8a6cf2c061094bda40d39244afdb6dffbc
xiemin <229356627@qq.com>
2019-11-18 21:55:15 +0800

    第一次2222

--- src/main/java/cn/itcast/controller/AAA.java
+++ src/main/java/cn/itcast/controller/AAA.java
@@ -4,6 +4,5 @@ public class AAA {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("aaaa1111");
-        System.out.println("4444444444");
     }
 }
